502db31dcb284a0917e7bd1e63c7256e897d00a1
seriab <aquinobritezz@gmail.com>
2022-02-21 16:17:31 -0300

    agregar algo de demo

--- albaranes_clientes/comprobarcliente_ini.php
+++ albaranes_clientes/comprobarcliente_ini.php
@@ -32,7 +32,7 @@ function limpiar() {
 		<? 
 	} else { ?>
 	<script>
-	alert ("No existe ningun cliente con ese codigo");
+	alert ("No existe ningun cliente con ese codigo 1237891011");
 	limpiar();
 	</script>
 	<? }
